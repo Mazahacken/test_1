3c689423b9e25203462d69e50bf93f94bc7a83de
Жека <skydiverjon@icloud.com>
2025-04-21 22:52:00 +0300

    Edite main.cpp

--- main.cpp
+++ main.cpp
@@ -4,8 +4,15 @@ template<typename T> T getSum(T x,  T y){
     return x + y;
 }
 
+
+
 auto main()-> int {
 
+    std::unique_ptr<int[]> ptrArr = std::make_unique<int[]>(6);
+    for (int i = 0; i < 6; ++i) {
+        ptrArr[i] = i;
+        std::cout << ptrArr[i] << std::endl;
+    }
     int a = 10;
     int b = 15;
     std::cout<<getSum<int>(a, b)<<std::endl;
