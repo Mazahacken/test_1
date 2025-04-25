006b6b769dd23dfddeb1d3b484290eacf8cfbf1e
Жека <skydiverjon@icloud.com>
2025-04-25 23:11:26 +0300

    Edite test.cpp

--- test1.cpp
+++ test1.cpp
@@ -1,5 +1,10 @@
 #include<iostream>
-
+class Int {
+protected:
+	int _date;
+public:
+	Int(int date) : _date(date) { std::cout << "Construct" << std::endl; }
+};
 auto main() -> int {
 
 	std::cout << "Test/n"<<std::endl;
