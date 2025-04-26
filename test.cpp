9f1315fe6d099c99ec712a30374d19ae176be598
Жека <skydiverjon@icloud.com>
2025-04-26 22:49:07 +0300

    Edite test.cpp

--- test.cpp
+++ test.cpp
@@ -6,7 +6,9 @@ template <typename T> T SumTest(T x, T y) {
 }
 
 auto main() -> int {
-
+	int x = 10;
+	int y = 5;
+	std::cout<<SumTest(x, y);
 	std::unique_ptr<int> ptr(new int(54));
 	std::unique_ptr<int> ptr2 = std::move(ptr);
 }
