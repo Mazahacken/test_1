86b15830a46fb000c696e73eb964b938bedb1f98
Жека <skydiverjon@icloud.com>
2025-04-26 22:41:51 +0300

    Edite test1.cpp

--- test1.cpp
+++ test1.cpp
@@ -4,6 +4,10 @@ protected:
 	int _date;
 public:
 	Int(int date) : _date(date) { std::cout << "Construct" << std::endl; }
+	~Int() { std::cout << "Destruct" << std::endl; }
+	int getDate() const { return _date; };
+	void setDate(int x) { this->_date = x; };
+
 };
 auto main() -> int {
 
