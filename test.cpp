9fe0c6378d34c1475b6d869e6023ea3204b08131
Жека <skydiverjon@icloud.com>
2025-06-04 23:52:20 +0300

    Edite test.cpp

--- test.cpp
+++ test.cpp
@@ -15,6 +15,6 @@ auto main() -> int {
 	std::shared_ptr<char> sharptr = std::make_shared<char>('H');
 	std::shared_ptr<int> sharptr2(new int(30));
 	std::unique_ptr<int[]> arr(new int[](5));
-	//
+	
 	
 }
